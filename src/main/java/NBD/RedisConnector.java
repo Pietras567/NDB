af1bdcf780906b0a31e000016581152294571181
247630 Bień Adrian <247630@edu.p.lodz.pl>
2024-11-22 11:42:25 +0100

    connection string brany z pliku config

--- src/main/java/NBD/RedisConnector.java
+++ src/main/java/NBD/RedisConnector.java
@@ -2,12 +2,28 @@ package NBD;
 
 import redis.clients.jedis.*;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class RedisConnector {
     private static JedisPooled pool;
+    private static final Properties property = new Properties();
+    private static final String confFile = "app.config";
+
+    private void readConfig() {
+        try (FileInputStream fis = new FileInputStream(confFile)) {
+            property.load(fis);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
     public void initConnection() {
+        this.readConfig();
         JedisClientConfig clientConfig = DefaultJedisClientConfig.builder().build();
-        pool = new JedisPooled(new HostAndPort("localhost", 6379), clientConfig);
+        pool = new JedisPooled(new HostAndPort(property.getProperty("app.address"),
+                Integer.parseInt(property.getProperty("app.port"))), clientConfig);
     }
 
     public RedisConnector() {
@@ -15,10 +31,10 @@ public class RedisConnector {
     }
 
     public void testConnection() {
-        String res1 = pool.set("bike:1", "Deimos");
+        String res1 = pool.set("bike:2", "cosus");
         System.out.println(res1); // OK
 
-        String res2 = pool.get("bike:1");
+        String res2 = pool.get("bike:2");
         System.out.println(res2); // Deimos
     }
 }
